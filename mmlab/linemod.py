27d4a89d86de0be2244663e0f135ec94d243aea9
LeoDuhz <1119653713@qq.com>
2021-06-02 16:28:33 +0800

    modify model train

--- mmlab/linemod.py
+++ mmlab/linemod.py
@@ -5,16 +5,32 @@ from PIL import Image
 from mmseg.datasets.builder import DATASETS
 from mmseg.datasets.custom import CustomDataset
 from mmcv import Config
+from mmseg.apis import set_random_seed
+from mmseg.datasets import build_dataset
+from mmseg.models import build_segmentor
+from mmseg.apis import train_segmentor
 
 #dirs config
-data_root = 'linemod'
+data_root = './linemod'
 img_dir = 'images'
 ann_dir = 'annotations'
 
 classes = ('ape', 'others')
 palette = [[255,255,255], [0,0,0]]
 
-#TODO:split train/val dataset randomly
+#split train/val dataset randomly
+split_dir = 'splits'
+mmcv.mkdir_or_exist(osp.join(data_root, split_dir))
+filename_list = [osp.splitext(filename)[0] for filename in mmcv.scandir(
+    osp.join(data_root, ann_dir), suffix='.png')]
+with open(osp.join(data_root, split_dir, 'train.txt'), 'w') as f:
+  # select first 4/5 as train set
+  train_length = int(len(filename_list)*4/5)
+  f.writelines(line + '\n' for line in filename_list[:train_length])
+with open(osp.join(data_root, split_dir, 'val.txt'), 'w') as f:
+  # select last 1/5 as train set
+  f.writelines(line + '\n' for line in filename_list[train_length:])
+
 
 #construct dataset class
 @DATASETS.register_module()
@@ -26,6 +42,106 @@ class LineModDataset(CustomDataset):
         super().__init__(img_suffix='.png', seg_map_suffix='.png', split=split, **kwargs)
         assert osp.exits(self.img_dir) and self.split is not None
 
+cfg = Config.fromfile('./fast_scnn.py')
+# Since we use ony one GPU, BN is used instead of SyncBN
+cfg.norm_cfg = dict(type='BN', requires_grad=True)
+cfg.model.backbone.norm_cfg = cfg.norm_cfg
+cfg.model.decode_head.norm_cfg = cfg.norm_cfg
+cfg.model.auxiliary_head[0].norm_cfg = cfg.norm_cfg
+cfg.model.auxiliary_head[1].norm_cfg = cfg.norm_cfg
+# modify num classes of the model in decode/auxiliary head
+cfg.model.decode_head.num_classes = 2
+cfg.model.auxiliary_head[0].num_classes = 2
+cfg.model.auxiliary_head[1].num_classes = 2
+
+cfg.dataset_type = 'LineModDataset'
+cfg.data_root = data_root
+
+cfg.data.samples_per_gpu = 8
+cfg.data.workers_per_gpu=8
+
+cfg.img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+cfg.crop_size = (512, 512)
+cfg.train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(640, 480), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=cfg.crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **cfg.img_norm_cfg),
+    dict(type='Pad', size=cfg.crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+cfg.test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(640, 480),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **cfg.img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+cfg.data.train.type = cfg.dataset_type
+cfg.data.train.data_root = cfg.data_root
+# cfg.data.train.img_dir = img_dir
+# cfg.data.train.ann_dir = ann_dir
+cfg.data.train.pipeline = cfg.train_pipeline
+cfg.data.train.split = 'splits/train.txt'
+
+cfg.data.val.type = cfg.dataset_type
+cfg.data.val.data_root = cfg.data_root
+# cfg.data.val.img_dir = img_dir
+# cfg.data.val.ann_dir = ann_dir
+cfg.data.val.pipeline = cfg.test_pipeline
+cfg.data.val.split = 'splits/val.txt'
+
+cfg.data.test.type = cfg.dataset_type
+cfg.data.test.data_root = cfg.data_root
+# cfg.data.test.img_dir = img_dir
+# cfg.data.test.ann_dir = ann_dir
+cfg.data.test.pipeline = cfg.test_pipeline
+cfg.data.test.split = 'splits/val.txt'
+
+# Set up working dir to save files and logs.
+cfg.work_dir = './work_dirs/'
+
+cfg.runner.max_iters = 200
+cfg.log_config.interval = 10
+cfg.evaluation.interval = 200
+cfg.checkpoint_config.interval = 200
+
+# Set seed to facitate reproducing the result
+cfg.seed = 0
+set_random_seed(0, deterministic=False)
+cfg.gpu_ids = range(1)
+
+# Let's have a look at the final config used for training
+print(f'Config:\n{cfg.pretty_text}')
+
+# Build the dataset
+datasets = [build_dataset(cfg.data.train)]
+
+# Build the detector
+model = build_segmentor(
+    cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
+# Add an attribute for visualization convenience
+model.CLASSES = datasets[0].CLASSES
+
+# Create work_dir
+mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+train_segmentor(model, datasets, cfg, distributed=False, validate=True, 
+                meta=dict())
 
 
     
---
+++ mmlab/test_linemod.py
@@ -0,0 +1,28 @@
+import mmcv
+import matplotlib.pyplot as plt
+import os.path as osp
+from PIL import Image
+import numpy as np
+# from mmseg.datasets.builder import DATASETS
+# from mmseg.datasets.custom import CustomDataset
+# from mmcv import Config
+# from mmseg.apis import set_random_seed
+import matplotlib.patches as mpatches
+
+classes = ('ape', 'others')
+palette = [[255,255,255], [0,0,0]]
+seg_map = Image.open('/home/zjunlict/dhz/NeuroRobotics/REDE/datasets/linemod/data/MilkBro/annotations/training/0000.png')
+seg_img = seg_map.convert('P')
+seg_img.putpalette(np.array(palette, dtype=np.uint8))
+# img = Image.open('/home/zjunlict/dhz/NeuroRobotics/REDE/datasets/linemod/data/MilkBro/annotations/training/0000.png')
+plt.figure(figsize=(8, 6))
+im = plt.imshow(np.array(seg_img.convert('RGB')))
+
+# create a patch (proxy artist) for every color 
+patches = [mpatches.Patch(color=np.array(palette[i])/255., 
+                          label=classes[i]) for i in range(2)]
+# put those patched as legend-handles into the legend
+plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., 
+           fontsize='large')
+
+plt.show()
